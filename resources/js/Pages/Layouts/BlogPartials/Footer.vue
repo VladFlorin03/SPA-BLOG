a9ebec57f003de0431e6ce35ce5c773da8b24c4f
VladFlorin03 <vladf03ac41@gmail.com>
2020-12-17 15:16:47 +0200

    Spa

---
+++ resources/js/Pages/Layouts/BlogPartials/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Footer -->
+    <footer class="blog-footer">
+        <p>SST</p>
+    </footer>
+    <!-- End Foooter -->
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
